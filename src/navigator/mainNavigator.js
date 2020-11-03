f1691677e84fa7275f5f4a13159badd940f40391
Crowdbotics <team@crowdbotics.com>
2020-11-03 08:30:26 +0000

    remove package blueprint-react-native-articles-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,7 +6,6 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import Dashboard13167075Navigator from '../features/Dashboard13167075/navigator';
-import ArticleList195776Navigator from '../features/ArticleList195776/navigator';
 
 /**
  * new navigators can be imported here
@@ -16,7 +15,6 @@ const AppNavigator = {
 
     //@BlueprintNavigationInsertion
 Dashboard13167075: { screen: Dashboard13167075Navigator },
-ArticleList195776: { screen: ArticleList195776Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
